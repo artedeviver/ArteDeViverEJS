42d0df1996c404082550c4c8d727aaded25519cc
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-11-12 13:16:19 -0300

    Adiciona template tela padrão dashboard

---
+++ user/User.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize")
+const connection = require("../database/database")
+
+const User = connection.define('users', {
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    responsibility:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    administrator:{
+        type: Sequelize.BOOLEAN,
+        allowNull:false
+    }
+})
+
+User.sync({force:false}).then(() =>{
+    console.log('Tabela de usuários criada')
+})
+
+module.exports = User
