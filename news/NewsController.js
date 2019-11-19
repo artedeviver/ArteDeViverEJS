0306843635427628c1914d008ce1f5c4681ddd43
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-11-19 11:11:12 -0300

    Adiciona excluir noticias

--- news/NewsController.js
+++ news/NewsController.js
@@ -2,20 +2,10 @@ const express = require("express")
 const router = express.Router()
 const News = require("./News")
 
-// app.get("/dashboard-noticias", (req,res) => {
-//     res.render("./dashboard/news/index")
-// })
-
-// app.get ("/dashboard/news/new", (req,res) =>{
-//     res.render("./dashboard/news/new")
-// })
-
-router.get("/dashboard/news", (req, res) => {
-    News.findAll().then(news => {
-        res.render("dashboard/news/index", { news: news })
-    })
+//ROTA PARA PÁGINA DE ADD NOVA NOTÍCIA
+router.get("/dashboard/news/new", (req, res) => {
+    res.render("./dashboard/news/new")
 })
-
 
 //SALVAR DADOS DO FORMULÁRIO - ADD NEWS
 router.post("/dashboard/news/save", (req, res) => {
@@ -29,10 +19,65 @@ router.post("/dashboard/news/save", (req, res) => {
         bodyNews: bodyNews,
         titleImage: titleImage
     }).then(() => {
-        res.redirect("./dashboard/news")
+        res.redirect("/dashboard/news")
     }).catch((error) => {
         res.send(error)
     })
 })
+
+// MOSTRAR TODAS AS NOTÍCIAS JA CADASTRADAS 
+// EM ORDEM DESCRECENTE
+router.get("/dashboard/news", (req, res) => {
+    News.findAll({
+        raw: true, order: [
+            ['id', 'DESC']
+        ]
+    }).then(news => {
+        res.render("dashboard/news/index", { news: news })
+    })
+})
+
+//DELETAR USUÁRIO
+router.post("/dashboard/news/delete", (req, res) => {
+    let id = req.body.id
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            News.destroy({
+                where: {id:id}
+            }).then(() => {
+                res.redirect("/dashboard/news")
+            })
+        }else{ //não é número
+            res.redirect("/dashboard/news")
+        }
+    }else{ //null
+        res.redirect("/dashboard/news")
+    }
+})
+
+// ROTA PARA PÁGINA DE EDIÇÃO DE USUÁRIO
+// router.get("/dashboard/news/edit/:id", (req, res) => {
+//     let id = req.params.id
+
+//     if (isNaN(id)) {
+//         res.redirect("./dashboard/news")
+//     }
+
+//     News.findByPk(id).then(news => {
+//         if (news != undefined) {
+//             res.render("./dashboard/news/edit", { news: news })
+//         } else {
+//             res.redirect("./dashboard/news")
+//         }
+//     }).catch(error => {
+//         res.redirect("./dashboard/news")
+//     })
+// })
+
+
+
+
+
 
 module.exports = router
