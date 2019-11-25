7f61878bc15f741291e7c1267560f13011f0417b
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-11-25 13:41:05 -0300

    Corrige rotas

---
+++ homepage/HomepageController.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const router = express.Router()
+
+//ROTAS PARA PROTEJOS
+router.get("/courses", (req, res) => {
+    res.render("./homepage/cursos")
+})
+
+//ROTA PARA QUEM SOMOS 
+router.get("/WhoWeAre", (req, res) => {
+    res.render("./homepage/quemSomos")
+})
+
+//ROTA PARA NOTÍCIAS
+router.get("/news", (req, res) => {
+    res.render("./homepage/news")
+})
+
+//ROTA PARA SEJA MEMBRO
+router.get("/beaMember", (req, res) => {
+    res.render("./homepage/sejaMembro")
+})
+
+module.exports = router
