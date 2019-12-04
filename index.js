21deb60001a8afb69791af70483245d4253f556d
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-12-04 10:36:50 -0300

    Adiciona rotas de login

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
+const session = require('express-session')
+const bcrypt = require('bcryptjs')
 
 const Homepage = require("./homepage/Homepage")
 const HomepageController = require("./homepage/HomepageController")
@@ -27,6 +29,12 @@ const PartnersController = require("./partners/PartnersController")
 
 // ejs
 app.set('view engine', 'ejs')
+
+//sessions
+app.use(session({
+    secret: "uhsuahsuashaushausuasuashushau", //pode ser qualquer texto, quanto mais aleatório, melhor
+    cookie: { maxAge: 1800000 }
+}))
 
 //arquivos staticos - css, javascrip, img
 app.use(express.static('public'))
@@ -60,24 +68,60 @@ app.use("/", PrincipalAreaController)
 app.use("/", PartnersController)
 
 
+
 //rota padrão
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-//rotas auxiliares
-// app.use("/", userController)
-app.get("/login", (req,res) =>{
+
+
+// rotas de login
+app.get("/login", (req, res) => {
     res.render("dashboard/login")
 })
 
+// validação de usuário
+app.post("/authenticate", (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user != undefined) {
+            let correct = bcrypt.compareSync(password, user.password)
+
+            if (correct) {
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.redirect("/dashboard")
+            } else {
+                res.redirect("/login")
+            }
+        } else {
+            res.redirect("/login")
+        }
+    })
+})
+
+//logout
+app.get("/logout", (re1, res) => {
+    req.session.user = undefined
+    req.redirect("/")
+})
+
+
+
+//rotas auxiliares
 app.get("/dashboard", (req, res) => {
     res.render("./dashboard/index")
 })
 
-app.get("/dashboard/home", (req,res) => {
-    res.render("./dashboard/home/index", {successEdit: req.query.successEdit})
+app.get("/dashboard/home", (req, res) => {
+    res.render("./dashboard/home/index", { successEdit: req.query.successEdit })
 })
+
 
 // server
 app.listen(4000, () => {
--- user/UserController.js
+++ user/UserController.js
@@ -1,23 +1,26 @@
 const express = require("express")
 const router = express.Router()
 const User = require("./User")
+const bcrypt = require('bcryptjs')
+const adminAuth = require('../middlewares/adminAuth')
+
 
 //MOSTRAR TODOS OS USUÁRIOS CADASTRADOS
-router.get("/dashboard/users", (req, res) => {
+router.get("/dashboard/users", adminAuth, (req, res) => {
     User.findAll({
         raw: true, order: [['id', 'DESC']]
     }).then(user => {
-        res.render("dashboard/user/index", { user: user, success: req.query.success, successEdit: req.query.successEdit})
+        res.render("dashboard/user/index", { user: user, success: req.query.success, successEdit: req.query.successEdit })
     })
 })
 
 //ROTA PARA PÁGINA DE ADD NOVO USUÁRIO
 router.get("/dashboard/users/new", (req, res) => {
-    res.render("./dashboard/user/new")
+    res.render("./dashboard/user/new", {success: req.query.success})
 })
 
 //SALVAR DADOS DO FORMULÁRIO - ADD USER
-router.post("/dashboard/users/save", (req, res) => {
+router.post("/dashboard/users/save", adminAuth, (req, res) => {
 
     let name = req.body.name
     let responsibility = req.body.responsibility
@@ -25,27 +28,36 @@ router.post("/dashboard/users/save", (req, res) => {
     let password = req.body.password
     let administrator = req.body.administrator
 
-    if (administrator == undefined) {
-        administrator = false
-    }
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user == undefined) {
+            let salt = bcrypt.genSaltSync(10)
+            let hash = bcrypt.hashSync(password, salt);
 
-    if (name != undefined && responsibility != undefined && email != undefined && password != undefined && administrator != undefined) {
-        User.create({
-            name: name,
-            responsibility: responsibility,
-            email: email,
-            password: password,
-            administrator: administrator
-        }).then((user) => {
-            res.redirect("/dashboard/users?success=true")
-        }).catch((error) => {
-            res.send(error)
-        })
-    }
+            if (administrator == undefined) {
+                administrator = false
+            }
+
+            if (name != undefined && responsibility != undefined && email != undefined && hash != undefined && administrator != undefined) {
+                User.create({
+                    name: name,
+                    responsibility: responsibility,
+                    email: email,
+                    password: hash,
+                    administrator: administrator
+                }).then((user) => {
+                    res.redirect("/dashboard/users?success=true")
+                }).catch((error) => {
+                    res.send(error)
+                })
+            }
+        } else {
+            res.redirect("/dashboard/users/new?success=false")
+        }
+    })
 })
 
 // ROTA PARA PÁGINA DE EDIÇÃO DE USUÁRIO
-router.get("/dashboard/users/edit/:id", (req, res) => {
+router.get("/dashboard/users/edit/:id", adminAuth, (req, res) => {
     let id = req.params.id
 
     if (isNaN(id)) {
@@ -64,7 +76,7 @@ router.get("/dashboard/users/edit/:id", (req, res) => {
 })
 
 //SALVAR DADOS DO FORMULÁRIO - UPDATE 
-router.post("/dashboard/users/update", (req, res) => {
+router.post("/dashboard/users/update", adminAuth, (req, res) => {
     let id = req.body.id
     let name = req.body.name
     let responsibility = req.body.responsibility
@@ -86,7 +98,7 @@ router.post("/dashboard/users/update", (req, res) => {
 })
 
 //DELETAR USUÁRIO
-router.post("/dashboard/users/delete", (req, res) => {
+router.post("/dashboard/users/delete", adminAuth, (req, res) => {
     let id = req.body.id
 
     if (id != undefined) {
@@ -102,24 +114,6 @@ router.post("/dashboard/users/delete", (req, res) => {
     } else { //null
         res.redirect("/dashboard/users")
     }
-})
-
-// LOGIN
-router.post("/login", (req, res) => {
-    let email = req.body.email
-    let password = req.body.password
-
-
-    User.findOne({
-        where: { email: email, password: password }
-    }).then(user => {
-
-        if (user && password == user.get('password') && email == user.get('email')) {
-            res.render("./dashboard/index")
-        } else {
-            res.send(console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"))
-        }
-    })
 })
 
 module.exports = router
