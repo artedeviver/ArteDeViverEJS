c420246cd0a283a318e51c83deb0611703f2cb00
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-12-11 10:05:50 -0300

    Corrige host

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 
 const connection = new Sequelize('arte_de_viver_bd', 'root', 'artedeviver', {
-    host: '10.10.104.92',
+    host: 'localhost',
     dialect: 'mysql'
 })
 
