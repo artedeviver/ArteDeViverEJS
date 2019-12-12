0dc5156d16dd9dc1b4cb17152d6cbabddad77799
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2019-12-12 13:10:43 -0300

    Adiciona editor de texto na página de notícias

--- public/js/newsValidation.js
+++ public/js/newsValidation.js
@@ -1,25 +1,29 @@
 let form = document.querySelector('.form-container')
 let btn = document.querySelector('.btn-change')
 let title = document.getElementsByName('title')
-let bodyNews = document.getElementsByName('bodyNews')
+let bodyNews = document.querySelector('.mce-content-body')
 let titleImage = document.getElementsByName('titleImage')
 let img = document.querySelector('.img-selected')
 
 
 form.addEventListener("blur", function (event) {
-    if (event.target.value == '') {
-        event.target.classList.add('error')
-        verify()
-    } else {
-        event.target.classList.remove('error')
-        verify()
-    }
+    console.log('ooooooooooooooooooooooooooooooooooooooooooooooooo')
+    // if (event.target.value == '') {
+    //     event.target.classList.add('error')
+    //     console.log('EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
+    //     // verify()
+    // } else {
+    //     event.target.classList.remove('error')
+    //     console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    //     // verify()
+    // }
 }, true);
 
-function verify() {
-    if (title[0].value != '' && bodyNews[0].value != '' && titleImage[0].value != '') {
-        btn.disabled = false
-        btn.classList.remove("btn-disabled")
-        btn.classList.add("primary")
-    }
-}
+// function verify() {
+//     if (title[0].value != '' && bodyNews.innerText != '' && titleImage[0].value != '') {
+//         // btn.disabled = false
+//         // btn.classList.remove("btn-disabled")
+//         // btn.classList.add("primary")
+//         console.log('aahaihauhusahsaishusshiahhshsauihsu')
+//     }
+// }
