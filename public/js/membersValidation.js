9b096c166cfcbb7635916ba46b87e1976f896e61
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-12-20 13:48:38 -0300

    Adiciona validacao dos campos

--- public/js/membersValidation.js
+++ public/js/membersValidation.js
@@ -2,8 +2,10 @@ let form = document.querySelector('.form-container')
 let btn = document.querySelector('.space-button')
 let name = document.getElementsByName('name')
 let interest = document.getElementById('interest')
-let collaborator = document.getElementsByName('collaborator')
-let age = document.getElementsByName('age')
+let typeActivity = document.getElementsByName('typeActivity')
+let materialDonate = document.getElementsByName('materialDonate')
+let valueDonate = document.getElementsByName('valueDonate')
+let nameInst =document.getElementsByName('nameInst')
 let email = document.getElementsByName('email')
 let telephone = document.getElementsByName('telephone')
 let reason = document.getElementsByName('reason')
@@ -22,7 +24,8 @@ form.addEventListener("blur", function (event) {
 
 function verify() {
     if (name[0].value != '' && email[0].value != '' && telephone[0].value != '' && reason[0].value != '') {
-        if (interest.value == 2 && collaborator[0].value != '' || interest.value == 1 && age[0].value != '') {
+        if (interest.value == 1 && typeActivity[0].value != '' || interest.value == 2 && materialDonate[0].value != '' ||
+        interest.value == 3 && valueDonate[0].value != 0 || interest.value == 4 && nameInst[0].value != '' ) {
 
             btn.disabled = false
             btn.classList.remove("btn-disabled")
